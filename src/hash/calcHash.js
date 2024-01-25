f84920e72ff59f27db0f6645a1ec575d965018a0
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-01-26 00:59:33 +0300

    feat: add hash code 26 янв 2024 г.  0:59:33

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(pathToFile);
+
+    return new Promise((resolve, reject) => {
+        stream.on('data', (data) => {
+            hash.update(data);
+        });
+    
+        stream.on('end', () => {
+            const fileHash = hash.digest('hex');
+            console.log(fileHash);
+            resolve();
+        });
+    
+        stream.on('error', (err) => {
+            reject(err);
+        });
+    });
 };
 
 await calculateHash();
