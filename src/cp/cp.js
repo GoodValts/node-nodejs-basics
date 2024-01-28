cf182f1cec4e6a70f5ee2ae0fb497bad5c2e47b1
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-01-28 23:42:02 +0300

    feat: add cp code 28 янв 2024 г. 23:42:02

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,36 @@
+import url from 'url';
+import path from 'path';
+import { spawn } from 'child_process';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    process.stdout.write('\x1b[32mProcess created!\x1b[30m\nType \'\x1b[35mCLOSE\x1b[30m\' to exit\x1b[37m\n\n');
+
+    const childPath = path.join(__dirname, 'files', 'script.js');
+    const childProcess = spawn('node', [childPath, ...args]);
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data);
+        if (data.toString().trim() === 'CLOSE') {
+            console.log('\n\x1b[32mClosed successfully!\x1b[37m');
+            process.exit();
+        }
+    });
+    
+    process.stdin.on('end', () => {
+        childProcess.stdin.end();
+    });
+
+    childProcess.stdout.on('data', (data) => {
+        process.stdout.write(data.toString());
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const someArgumentsArr = [];
+
+for (let i = 0; i < 5; i++) {
+    someArgumentsArr.push(`someString${i.toString()}`);
+}
+
+spawnChildProcess(someArgumentsArr);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,9 +4,7 @@ const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 
 
 const sendResult = () => {
     parentPort.on('message', (data) => {
-        // console.log('ws data=', data);
         const result = nthFibonacci(data);
-        // console.log('ws res=', result);
         parentPort.postMessage(result);
     });
 };
